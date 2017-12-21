a889ed0f399257b40165936a6a381668e0774436
Dmitry Sushilov <iamdimka@me.com>
2017-12-21 14:56:55 +0200

    added popular languages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # @iamdimka/l10n
 
+Plural forms source is [docs.translatehouse.org](http://docs.translatehouse.org/projects/localization-guide/en/latest/l10n/pluralforms.html)
--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,37 @@ export function translate(dict: { [key: string]: string }, key: string, vars: { 
   }
 
   return result
-    .replace(/\[@([a-z0-9_]+)\|(.+?)]/ig, (found, key, choices) => plural(dict.code, vars[key] as number || 0, choices.split("|")))
+    .replace(/\[@([a-z0-9_]+)\|(.+?)]/ig, (found, key, choices) => {
+      const choice = plural(dict.code, vars[key] as number || 0)
+      const values = choices.split("|")
+
+      return  values[choice < values.length ? choice : 0]
+    })
     .replace(/@([a-z0-9_]+)/ig, (found, key) => vars[key] as string)
 }
 
-export function plural(lang: string, value: number, choices: string[]): string {
-  let i = 0
-  let expected = 2
-
+export function plural(lang: string, n: number): number {
   switch (lang) {
     case "uk":
+    case "be":
+    case "bs":
+    case "sr":
     case "ru":
-      i = (value % 10 === 1 && value % 100 != 11) ? 0 : ((value % 10 >= 2 && value % 10 <= 4 && (value % 100 < 10 || value % 100 >= 20)) ? 1 : 2)
-      expected = 3
-      break
+    case "hr":
+      return n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
 
-    default: //en
-      i = value === 1 ? 0 : 1
-      break
+    case "fr":
+      return n > 1 ? 1 : 0
+
+    case "ja":
+    case "zh-hant":  
+    case "zh-hans":  
+    case "zh-cn":  
+    case "zh-hk":  
+    case "zh-tw":
+      return 0  
+
+    default:
+      return n === 1 ? 0 : 1
   }
-
-  if (choices.length > expected) {
-    return value === 0 ? choices[0] : choices[i + 1]
-  }
-
-  return choices[i]
 }
